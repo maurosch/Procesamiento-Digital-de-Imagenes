d00d5435aea85a89e4ea15ac5d5e36033fc03caf
Mauro Schiavinato <maurolschiavinato@gmail.com>
2021-04-09 16:41:08 -0300

    Restructuro archivos; agrego ejercicios

--- Guias/1 - Operadores puntuales e histograma/.ipynb_checkpoints/1-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-img=mpimg.imread('image_name.png')
-
-def compresionRangoDinamicoGrises():
-    #Buscamos el máximo nivel de gris
--- Guias/util.py
+++
@@ -1,6 +0,0 @@
-def calcNivelesGrises(img):
-    L = 0
-    for x in range(len(img)):
-        for y in range(len(img[0])):
-            L = max(L, img[x][y]) 
-    return L+1
